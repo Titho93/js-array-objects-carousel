8820d24fa8d7bcf343b7ed34d88054fb49672024
Titho93 <titho10.93@gmail.com>
2023-09-22 16:35:11 +0200

    [add] ciclo forEach for photo elements

--- js/script.js
+++ js/script.js
@@ -31,21 +31,11 @@ const images = [
   }
 ];
 
-// const images  = [
-//   'img/01.webp',
-//   'img/02.webp',
-//   'img/03.webp',
-//   'img/04.webp',
-//   'img/05.webp'
-// ];
-
-
-for (let i = 0; i < images.length; i++){
-  const image = images[i];
-  imageCont.innerHTML += `<img src="${image}" class="item hide">`;
-  thumb.innerHTML += `<img src="${image}" class="object">`;
-}
-
+images.forEach( (photo) => {
+  console.log(photo)
+  imageCont.innerHTML += `<img src="${photo.image}" class="item hide">`;
+  thumb.innerHTML += `<img src="${photo.image}" class="object">`;
+}) 
 
 const imageCollect = document.getElementsByClassName('item');
 const thumbCollect = document.getElementsByClassName('object');
